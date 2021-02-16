2f242f02f2bd5e7fc1036c1010c960d82a122247
Dmitry Kokurkin <wolfyteze@gmail.com>
2021-02-16 11:59:25 +0300

    Initial Commit

---
+++ MailApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MailApp
+//
+//  Created by wolfyteze on 16/02/2021.
+//  Copyright © 2021 wolfyteze. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
